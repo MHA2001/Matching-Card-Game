ee2ba4b51a6c28d84a04f98d8c0b729593a81214
MHA2001 <mha.akhoundi@gmail.com>
2023-07-13 18:38:46 +0330

    END

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 let choose = [];
+let score = 0;
 
 function playGame() {
   cardRandomize();
@@ -9,6 +10,7 @@ function playGame() {
 }
 
 function cardRandomize() {
+
   let cards = document.querySelectorAll(".card img:first-child");
   let usedCard = [];
   cards.forEach(function (element) {
@@ -43,20 +45,21 @@ function turnBackCard(domArray, x) {
   }, x);
 }
 
-function flipCard() {
+function flipCard() { 
   if (choose.length != 2) {
     this.querySelector("img:first-child").classList.remove("front-of-card-of");
     this.querySelector("img:last-child").classList.remove("back-of-card-on");
 
     choose.push(this);
-    if (choose.length == 2) {
+
+    if (choose.length === 2) {
       if (isMatch(choose)) {
         choose.length = 0;
       } else {
-        turnBackCard(choose, 1500);
+        turnBackCard(choose, 800);
         setTimeout(() => {
           choose.length = 0;
-        }, 1500);
+        }, 800);
       }
     }
   }
@@ -65,6 +68,17 @@ function flipCard() {
 function isMatch(array) {
   let img2 = array[1].querySelector("img:first-child");
   let img1 = array[0].querySelector("img:first-child");
-  return img2.getAttribute("src") == img1.getAttribute("src");
+
+  let flag = img2.getAttribute("src") == img1.getAttribute("src");
+  if (flag) {
+    array[0].removeEventListener("click", flipCard);
+    array[1].removeEventListener("click", flipCard);
+    score++;
+    document.getElementById("score").textContent = score;
+  }
+  return flag;
 }
+
+
+
 playGame();
